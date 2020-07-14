2db6fe72166f7a667a0b59212621f15cb2466af3
Brian Clayton <finalb@projectfinalboss.com>
2020-07-14 18:26:25 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const Posts = require("./posts/posts-model.js");
 const postsRouter = require("./posts/posts-router.js");
@@ -7,9 +8,27 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/users', (req, res) =>{
+    const userInfo =  req.body;
+ 
+     userInfo.id = shortid.generate();
+     user.push(userInfo);
+ try {
+     res.status(201).json(userInfo)
+ } catch (err){
+     res.status(400).json({message: "errorMessage:", err});
+ }
+ });
+
 server.get("/api/posts", (req, res) => {
 
 })
 
 server.use("/api/posts", postsRouter);
+
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log('listening on localhost:', PORT);
+});
 
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
+server.post('/api/posts', (req, res) =>{
+    const postInfo =  req.body;
+ 
+     postInfo.id = shortid.generate();
+     user.push(postInfo);
+ try {
+     res.status(201).json(postInfo)
+ } catch (err){
+     res.status(400).json({message: "errorMessage:", err});
+ }
+ });
+
 server.get("/api/posts", (req, res) => {
     Posts.find(req.query)
     .then(post => {
