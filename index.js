dfbfa34313a837ae575ec03af9cbfad13596551f
Brian Clayton <finalb@projectfinalboss.com>
2020-07-14 19:01:15 -0400

    fix

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 const express = require("express");
-const shortid = require("shortid");
 
-const Posts = require("./posts/posts-model.js");
-const postsRouter = require("./posts/posts-router.js");
-
-const server = express();
-
-server.use(express.json());
-
-server.post('/api/users', (req, res) =>{
-    const userInfo =  req.body;
- 
-     userInfo.id = shortid.generate();
-     user.push(userInfo);
- try {
-     res.status(201).json(userInfo)
- } catch (err){
-     res.status(400).json({message: "errorMessage:", err});
- }
- });
-
-server.get("/api/posts", (req, res) => {
-
-})
-
-server.use("/api/posts", postsRouter);
+const postsRouter = require("./server.js");
 
 
 const PORT = 5000;
-server.listen(PORT, () => {
-    console.log('listening on localhost:', PORT);
+postsRouter.listen(PORT, () => {
+    console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
-
--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const shortid = require("shortid");
 
-const router = express.Router();
+const server = express();
+
+const pos = []
+
+
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) =>{
+    const userInfo =  req.body;
+ 
+     userInfo.id = shortid.generate();
+     pos.push(userInfo);
+ try {
+     res.status(201).json(userInfo)
+ } catch (err){
+     res.status(400).json({message: "errorMessage:", err});
+ }
+ });
+
+server.get("/", (req, res) => {
+    res.send(`
+    
+    `);
+})
+
+server.use("/api/posts", postsRouter);
+
 
 server.post('/api/posts', (req, res) =>{
     const postInfo =  req.body;
  
      postInfo.id = shortid.generate();
-     user.push(postInfo);
+     pos.push(postInfo);
  try {
      res.status(201).json(postInfo)
  } catch (err){
@@ -38,4 +66,5 @@ server.get("/api/posts", (req, res) => {
     })
 }) 
 
-module.exports = router;
+
+module.exports = server;
