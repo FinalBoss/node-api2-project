79129d8114baa6d361b25b67d2b3a2ba01ff11af
Brian Clayton <finalb@projectfinalboss.com>
2020-07-14 22:07:22 -0400

    Update Post Router

---
+++ posts/posts-router.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const shortid = require("shortid");
+
+
+const router = express.Router();
+
+
+router.post('/api/posts', (req, res) =>{
+    Posts.add(req.body)
+    .then(pos => {
+       res.status(201).json(pos);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Error adding the post', err})
+    })
+ });
+
+
+router.get("/api/posts", (req, res) => {
+    Posts.find(req.query)
+    .then(post => {
+        res.status(200).json(post);
+    }) 
+    .catch(err => {
+        res.status(500).json({
+            message: "Error retrieving posts", err
+        });
+    })
+}) 
+
+router.get("/api/posts/:id", (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        res.status(200).json(post);
+    }) 
+    .catch(err => {
+        res.status(500).json({
+            message: "Error retrieving posts", err
+        });
+    })
+}) 
+
+router.delete('/api/posts/:id', (req, res) => {
+    Posts.remove(req.params.id)
+    .then(count => {
+        if(count > 0){
+            res.status(200).json({ message: "The post is deleted"});
+        } else {
+            res.status(404).json({ message: "The Post could not be found"});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Error removing the Post', err})
+    })
+})
+
+
+router.put('/api/posts/:id', (req, res) => {
+    const changes = req.body;
+
+    Posts.update(req.params.id, changes)
+    .then(pos => {
+        if (pos){
+            res.status(200).json(pos);
+        } else {
+            res.status(404).json({ message: "The Post could not be found"})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "error updating the post"
+        })
+    })
+})
+
+router.get("/api/posts/:id/comments", async (req, res) => {
+    try {
+        const comments = await Posts.findPostComments(req.params.id);
+
+        if(comments.lenght > 0) {
+            res.status(200).json(comments);
+        } else {
+            res.status(404).json({ message: "No Comments for this Post"})
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Error retrieving the Comments for this Post"
+        })
+    }
+})
+
+router.post('/api/posts/:id/comments', async (req, res) =>{
+    const postInfo =  {...req.body, post_id: req.params.id };
+ try {
+  const comments = await Posts.addMessage(postInfo);
+    res.status(201).json(comments)
+ } catch (err){
+     res.status(500).json({message: "errorMessage:", err});
+ }
+ });
+
+
+
+ module.exports = router;
--- server.js
+++ server.js
@@ -1,73 +1,26 @@
 const express = require("express");
-const shortid = require("shortid");
 
 const server = express();
 
 const postsRouter = require('./posts/posts-router.js');
 const contentsRouter = require('./contents/contnets-router.js');
 
-const pos = []
-
-
 
 server.use(express.json());
 
-server.post('/api/users', (req, res) =>{
-    const userInfo =  req.body;
- 
-     userInfo.id = shortid.generate();
-     pos.push(userInfo);
- try {
-     res.status(201).json(userInfo)
- } catch (err){
-     res.status(400).json({message: "errorMessage:", err});
- }
- });
 
 server.get("/", (req, res) => {
     res.send(`
-    
+    <p>Hello World</p>
     `);
 })
 
 server.use("/api/posts", postsRouter);
+server.use("/api/posts/:id/comments", contentsRouter);
 
 
-server.post('/api/posts', (req, res) =>{
-    const postInfo =  req.body;
- 
-     postInfo.id = shortid.generate();
-     pos.push(postInfo);
- try {
-     res.status(201).json(postInfo)
- } catch (err){
-     res.status(400).json({message: "errorMessage:", err});
- }
- });
 
-server.get("/api/posts", (req, res) => {
-    Posts.find(req.query)
-    .then(post => {
-        res.status(200).json(post);
-    }) 
-    .catch(err => {
-        res.status(500).json({
-            message: "Error retrieving posts", err
-        });
-    })
-}) 
 
-server.get("/api/posts", (req, res) => {
-    Posts.findById(req.params.id)
-    .then(post => {
-        res.status(200).json(post);
-    }) 
-    .catch(err => {
-        res.status(500).json({
-            message: "Error retrieving posts", err
-        });
-    })
-}) 
 
 
 module.exports = server;
